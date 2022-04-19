bba2f2a1128594c96897b1fd474b1ac00e5e0942
Jason Cheung <yongadorjechang@gmail.com>
2022-04-19 08:10:45 +0800

    Some refinment

--- password-retry.py
+++ password-retry.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 i = 3 # number of chance left
 while i > 0:
+	i = i - 1
 	pwd = input('Please enter password: ')
 	if pwd == password:
 		print('Login Success!')
 		break # Escape from loop
 	else:
-		i = i - 1
-		print('Wrong password!', i , 'more chance')
+		print('Wrong password!')
+		if i > 0:
+			print('Wrong password!', i , 'more chance')
+		else:
+			print('No more chance to enter! Locking account!')
